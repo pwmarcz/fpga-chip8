742c643ff421eddf661a3e6519adb0f3a08c6517
Paweł Marczewski <pwmarcz@gmail.com>
2018-12-10 20:29:38 +0100

    Move assembler files to asm/

--- project.mk
+++ project.mk
@@ -2,11 +2,11 @@
 # pip3 install --user 'git+https://github.com/aanunez/tortilla8.git
 # sudo apt install python3-tk
 
-TESTS = $(patsubst %.c8asm,build/%.hex,$(wildcard *.c8asm))
+TESTS = $(patsubst asm/%.c8asm,build/%.hex,$(wildcard asm/*.c8asm))
 
 build/cpu_tb.out: $(TESTS)
 
-build/%.ch8: %.c8asm
+build/%.ch8: asm/%.c8asm
 	tortilla8 assemble $< -o $(@:.ch8=)
 
 build/%.hex: build/%.ch8
