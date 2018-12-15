8d069d227e9b8b1b7330a55a4fa588277f23d3f6
Paweł Marczewski <pwmarcz@gmail.com>
2018-12-15 11:55:23 +0100

    Update clock speed for BX

--- chip8.v
+++ chip8.v
@@ -51,7 +51,7 @@ module top(input wire CLK,
   reg [18:0] counter;
   wire tick_60hz = counter == 0;
 
-  parameter clk_freq = 12_000_000;
+  parameter clk_freq = 16_000_000;
   parameter clk_divider = clk_freq / 60;
 
   cpu cpu0(CLK, tick_60hz, LED);
